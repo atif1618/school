2693b012c454442e9388076086ab72630282bc83
wasif <wasifbaloch527@gmail.com>
2024-06-12 18:13:54 +0500

    update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\index;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
@@ -13,3 +14,10 @@ Route::view('profile', 'profile')
     ->name('profile');
 
 require __DIR__.'/auth.php';
+
+
+
+Route::group(['middleware'=>['auth']],function(){
+    Route::get('index', index::class)->name('index');
+
+});
